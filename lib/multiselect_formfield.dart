a951408a3d45767b37899dfb636d4faa1971bbeb
thoffart <thiagohoffart321920@gmail.com>
2019-11-07 18:15:17 -0300

    small changes

--- lib/multiselect_formfield.dart
+++ lib/multiselect_formfield.dart
@@ -6,13 +6,14 @@ import 'package:multiselect_formfield/multiselect_dialog.dart';
 class MultiSelectFormField extends FormField<dynamic> {
   final String titleText;
   final String hintText;
-  final bool required;
   final String errorText;
   final dynamic value;
   final List dataSource;
   final String textField;
+  final IconData icon;
   final String valueField;
   final Function change;
+  final Function clear;
   final Function open;
   final Function close;
   final Widget leading;
@@ -20,135 +21,143 @@ class MultiSelectFormField extends FormField<dynamic> {
   final String okButtonLabel;
   final String cancelButtonLabel;
 
-  MultiSelectFormField(
-      {FormFieldSetter<dynamic> onSaved,
-      FormFieldValidator<dynamic> validator,
-      int initialValue,
-      bool autovalidate = false,
-      this.titleText = 'Title',
-      this.hintText = 'Tap to select one or more',
-      this.required = false,
-      this.errorText = 'Please select one or more options',
-      this.value,
-      this.leading,
-      this.dataSource,
-      this.textField,
-      this.valueField,
-      this.change,
-      this.open,
-      this.close,
-      this.okButtonLabel = 'OK',
-      this.cancelButtonLabel = 'CANCEL',
-      this.trailing})
-      : super(
-          onSaved: onSaved,
-          validator: validator,
-          initialValue: initialValue,
-          autovalidate: autovalidate,
-          builder: (FormFieldState<dynamic> state) {
-            List<Widget> _buildSelectedOptions(dynamic values, state) {
-              List<Widget> selectedOptions = [];
-
-              if (values != null) {
-                values.forEach((item) {
-                  var existingItem = dataSource.singleWhere((itm) => itm[valueField] == item, orElse: () => null);
-                  selectedOptions.add(Chip(
-                    label: Text(existingItem[textField], overflow: TextOverflow.ellipsis),
-                  ));
-                });
-              }
-
-              return selectedOptions;
-            }
-
-            return InkWell(
-              onTap: () async {
-                List initialSelected = value;
-                if (initialSelected == null) {
-                  initialSelected = List();
-                }
-
-                final items = List<MultiSelectDialogItem<dynamic>>();
-                dataSource.forEach((item) {
-                  items.add(MultiSelectDialogItem(item[valueField], item[textField]));
-                });
-
-                List selectedValues = await showDialog<List>(
-                  context: state.context,
-                  builder: (BuildContext context) {
-                    return MultiSelectDialog(
-                      title: titleText,
-                      okButtonLabel: okButtonLabel,
-                      cancelButtonLabel: cancelButtonLabel,
-                      items: items,
-                      initialSelectedValues: initialSelected,
-                    );
-                  },
-                );
-
-                if (selectedValues != null) {
-                  state.didChange(selectedValues);
-                  state.save();
-                }
-              },
-              child: InputDecorator(
-                decoration: InputDecoration(
-                  filled: true,
-                  errorText: state.hasError ? state.errorText : null,
-                  errorMaxLines: 4,
+  MultiSelectFormField({
+    FormFieldSetter<dynamic> onSaved,
+    FormFieldValidator<dynamic> validator,
+    int initialValue,
+    bool autovalidate = false,
+    this.titleText = 'Title',
+    this.hintText = 'Tap to select one or more',
+    this.errorText = 'Please select one or more options',
+    this.value,
+    this.leading,
+    this.dataSource,
+    this.textField,
+    this.icon,
+    this.valueField,
+    this.change,
+    this.clear,
+    this.open,
+    this.close,
+    this.okButtonLabel = 'OK',
+    this.cancelButtonLabel = 'CANCEL',
+    this.trailing
+  }) : super(
+    onSaved: onSaved,
+    validator: validator,
+    initialValue: initialValue,
+    autovalidate: autovalidate,
+    builder: (FormFieldState<dynamic> state) {
+      List<Widget> _buildSelectedOptions(dynamic values, state) {
+        final List<Widget> selectedOptions = [];
+        if (values != null) {
+          values.forEach((item) {
+            final existingItem = dataSource.singleWhere((itm) => itm[valueField] == item, orElse: () => null);
+            selectedOptions.add(
+              Chip(
+                label: Text(existingItem[textField], overflow: TextOverflow.ellipsis),
+                backgroundColor: Colors.white,
+                shape:  RoundedRectangleBorder(
+                  side: BorderSide(color: Color.fromRGBO(3, 78, 162, 1), width: 1),
+                  borderRadius: BorderRadius.circular(40.0),
                 ),
-                isEmpty: state.value == null || state.value == '',
-                child: Column(
+              )
+            );
+          });
+        }
+        return selectedOptions;
+      }
+      return InkWell(
+        onTap: () async {
+          List initialSelected = value;
+          if (initialSelected == null) {
+            initialSelected = List();
+          }
+
+          final items = List<MultiSelectDialogItem<dynamic>>();
+          dataSource.forEach((item) {
+            items.add(MultiSelectDialogItem(item[valueField], item[textField]));
+          });
+
+          List selectedValues = await showDialog<List>(
+            context: state.context,
+            builder: (BuildContext context) {
+              return MultiSelectDialog(
+                title: titleText,
+                okButtonLabel: okButtonLabel,
+                cancelButtonLabel: cancelButtonLabel,
+                items: items,
+                initialSelectedValues: initialSelected,
+              );
+            },
+          );
+
+          if (selectedValues != null) {
+            state.didChange(selectedValues);
+            state.save();
+          }
+        },
+        child: InputDecorator(
+          decoration: InputDecoration(
+            filled: false,
+            errorText: state.hasError ? state.errorText : null,
+            errorMaxLines: 4,
+            icon: icon == null ? null : Icon(icon, color: Colors.grey),
+            suffixIcon: (clear != null && value != null && value.length > 0)
+              ? InkWell(
+                onTap:() {
+                  clear();
+                },
+                child: Icon(
+                  Icons.close,
+                  color: Colors.grey,
+                  size: 25.0,                            
+                ),
+              )
+              : Icon(
+                Icons.arrow_drop_down,
+                color: Colors.grey,
+                size: 25.0,                            
+              )
+          ),
+          isEmpty: state.value == null || state.value == '',
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
+                child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Expanded(
-                              child: Text(
-                            titleText,
-                            style: TextStyle(fontSize: 13.0, color: Colors.black54),
-                          )),
-                          required
-                              ? Padding(padding:EdgeInsets.only(top:5, right: 5), child: Text(
-                                  ' *',
-                                  style: TextStyle(
-                                    color: Colors.red.shade700,
-                                    fontSize: 17.0,
-                                  ),
-                                ),
-                              )
-                              : Container(),
-                          Icon(
-                            Icons.arrow_drop_down,
-                            color: Colors.black87,
-                            size: 25.0,                            
+                    value != null && value.length > 0
+                    ? Expanded(
+                      child: Wrap(
+                        spacing: 8.0,
+                        runSpacing: 0.0,
+                        children: _buildSelectedOptions(value, state),
+                      )
+                    )
+                    : Expanded(
+                      child: Container(
+                        padding: EdgeInsets.only(top: 4, left: 12),
+                        child: Text(
+                          hintText,
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
                           ),
-                        ],
+                        ),
                       ),
                     ),
-                    value != null && value.length > 0
-                        ? Wrap(
-                            spacing: 8.0,
-                            runSpacing: 0.0,
-                            children: _buildSelectedOptions(value, state),
-                          )
-                        : new Container(
-                            padding: EdgeInsets.only(top: 4),
-                            child: Text(
-                              hintText,
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.grey.shade500,
-                              ),
-                            ),
-                          )
                   ],
                 ),
               ),
-            );
-          },
-        );
+              
+            ],
+          ),
+        ),
+      );
+    },
+  );
 }
